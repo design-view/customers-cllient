13656c88fc3aee075c4877b10e392ec7ac50c4c7
pink <pink@naver.com>
2022-01-19 17:47:38 +0900

    고객등록,삭제 완료

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import CustomerList from './components/CustomerList';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CreateCustomer from './components/CreateCustomer';
+import DetailCustomer from './components/DetailCustomer';
 import { Route, Routes } from 'react-router-dom';
+import Test from './components/Test';
 
 function App() {
   const title = "그린고객관리";
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<CustomerList />} />
           <Route path="/create" element={<CreateCustomer />} />
+          <Route path="/customer/:id" element={<DetailCustomer />} />
         </Routes>
       </div> 
       <Footer title={title}/>
--- src/components/CreateCustomer.js
+++ src/components/CreateCustomer.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import { Table, TableBody, TableCell, TableRow } from '@material-ui/core';
-function CreateCustomer(props) {
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+function CreateCustomer() {
+    const navigate = useNavigate();
+    const [ formData, setFormData ] = useState({
+        c_name:"",
+        c_phone:"",
+        c_birthday:"",
+        c_gender:"",
+        c_addr:""
+    })
+    const onChange = (e) => {
+        const { name,  value } = e.target;
+        setFormData({
+            ...formData,
+            [name]:value
+        })
+        console.log(name, value);
+    }
+    //폼 submit이벤트
+    const onSubmit = (e) => {
+        e.preventDefault();
+        insertCustomer();
+        setFormData({
+            c_name:"",
+            c_phone:"",
+            c_birthday:"",
+            c_gender:"",
+            c_addr:""
+        })
+    }
+    //post전송 axios
+    function insertCustomer(){
+        axios.post("http://localhost:8080/addCustomer",formData)
+        .then(function(res){
+            console.log(res);
+            navigate(-1);
+        }).catch(function(err){
+            console.log(err);
+        })
+    }
     return (
         <div>
             <h2>신규 고객 등록하기</h2>
-            <form>
+            <form onSubmit={onSubmit}>
                 <Table className='createTable'>
                     <TableBody>
                         <TableRow>
@@ -12,7 +53,7 @@ function CreateCustomer(props) {
                                 이름
                             </TableCell>
                             <TableCell>
-                                <input name="cname" type="text" />
+                                <input name="c_name" type="text" value={formData.c_name} onChange={onChange} />
                             </TableCell>
                         </TableRow>
                         <TableRow>
@@ -20,7 +61,7 @@ function CreateCustomer(props) {
                                 연락처
                             </TableCell>
                             <TableCell>
-                                <input name="cphone" type="text" />
+                                <input name="c_phone" value={formData.c_phone} type="text" onChange={onChange} />
                             </TableCell>
                         </TableRow>
                         <TableRow>
@@ -28,7 +69,7 @@ function CreateCustomer(props) {
                                 생년월일
                             </TableCell>
                             <TableCell>
-                                <input name="cbirthday" type="date" />
+                                <input name="c_birthday" type="date" value={formData.c_birthday} onChange={onChange}  />
                             </TableCell>
                         </TableRow>
                         <TableRow>
@@ -36,8 +77,8 @@ function CreateCustomer(props) {
                                 성별
                             </TableCell>
                             <TableCell>
-                                여성 <input name="cgender" type="radio" value="여성" />
-                                남성 <input name="cgender" type="radio" value="남성" />
+                                여성 <input name="c_gender" type="radio" value="여성" onChange={onChange} />
+                                남성 <input name="c_gender" type="radio" value="남성" onChange={onChange} />
                             </TableCell>
                         </TableRow>
                         <TableRow>
@@ -45,7 +86,7 @@ function CreateCustomer(props) {
                                 주소
                             </TableCell>
                             <TableCell>
-                                <input name="addr" type="text" />
+                                <input name="c_addr" type="text" value={formData.c_addr} onChange={onChange}  />
                             </TableCell>
                         </TableRow>
                         <TableRow>
